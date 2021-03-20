3d12334a439e8013dde4becb71fbebfdffe07970
ziyi3.zhang <ziyi3.zhang@zhangmen.com>
2021-03-20 16:06:13 +0800

    2021-03-20

--- Challenges/src/p1302_DeepestLeavesSum/Solution.java
+++ Challenges/src/p1302_DeepestLeavesSum/Solution.java
@@ -8,7 +8,30 @@ package p1302_DeepestLeavesSum;
  * Given the root of a binary tree, return the sum of values of its deepest leaves.
  */
 class Solution {
-    public int deepestLeavesSum(TreeNode root) {
 
+    int res = 0;
+    int maxDepth = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) return 0;
+        process(root, 0);
+        return res;
+    }
+
+    public void process(TreeNode root, int depth) {
+        if (root.left == null && root.right == null) {
+            if (depth > maxDepth) {
+                maxDepth = depth;
+                res = root.val;
+            } else if (depth == maxDepth) {
+                res += root.val;
+            }
+        }
+        if (root.left != null) {
+            process(root.left, depth + 1);
+        }
+        if (root.right != null) {
+            process(root.right, depth + 1);
+        }
     }
 }
